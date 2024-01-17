d9a9a88882a5c52535022d67908de97d2f532b9b
riteshk_007 <codeshorts@gmail.com>
2024-01-17 16:59:54 +0530

    Add AllBlogs component to Home page

---
+++ app/(router)/home/_components/AllBlogs.jsx
@@ -0,0 +1,19 @@
+import BlogCard from "./BlogCard"
+
+
+const AllBlogs = () => {
+  return (
+    <div className=" bg-gray-100 rounded-xl p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+    </div>
+  )
+}
+
+export default AllBlogs
--- app/(router)/home/page.js
+++ app/(router)/home/page.js
@@ -1,19 +1,21 @@
+import AllBlogs from "./_components/AllBlogs"
 import WelcomeBanner from "./_components/WelcomeBanner"
 
 
-const Courses = () => {
+const Home = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 p-5">
     {/* left container */}
      <div className="col-span-2">
      <WelcomeBanner/>
+     <AllBlogs/>
      </div>
        {/* right container */}
      <div>
-      right container
+     
      </div>
     </div>
   )
 }
 
-export default Courses
+export default Home
