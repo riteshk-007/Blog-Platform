4a5cf14cd34f82da3e07ec7c00c9087d79f1d6bb
riteshk_007 <codeshorts@gmail.com>
2024-01-17 16:34:34 +0530

    Update WelcomeBanner styling

--- app/(router)/home/_components/WelcomeBanner.jsx
+++ app/(router)/home/_components/WelcomeBanner.jsx
@@ -7,10 +7,10 @@ const WelcomeBanner = () => {
     {/* image  */}
     <Image src={"/panda.png"} width={80} height={70} alt="panda"  className="m-2"/>
     <div>
-        <h2 className="font-bold text-4xl">
+        <h2 className="font-bold text-base md:text-xl lg:text-4xl">
             Welcome to <span className="text-indigo-600">Blog Platform</span>
         </h2>
-        <p className="text-base text-gray-500">
+        <p className="text-sm md:text-base text-gray-500">
           Explore blogs from different categories
         </p>
     </div>
