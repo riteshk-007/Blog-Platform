09ff4c9666278b23bd4c6b542be2f2621b618c8b
riteshk-007 <codeshorts007@gmail.com>
2024-01-19 15:01:38 +0530

    Update user authentication logic

---
+++ app/api/(user)/login/route.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+import prisma from "@/DB/db.config";
+export const POST =async (req)=>{
+    try {
+        const {email, password} =  await req.json();
+        if([email, password].some((filed) => filed?.trim() === "")){
+            return NextResponse.json({message : 'Please fill all the fields', status : 400})
+        }
+        const user = await prisma.user.findUnique({
+            where:{
+                email : email
+            }
+        })
+        if(!user){
+            return NextResponse.json({message : 'User not found', status : 400})
+        }else{
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(!isMatch){
+                return NextResponse.json({message : 'Invalid password', status : 400})
+            }
+            const authToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+          cookies().set("authToken", authToken, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7,
+          });
+          return NextResponse.json({
+            status : 200,
+            message : 'User logged in successfully',
+            data : user
+          })
+        }
+    } catch (error) {
+        return NextResponse.json({message : error.message || "Something went wrong", status : 400})
+        
+    }
+}
