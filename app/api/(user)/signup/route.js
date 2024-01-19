d74fa3304b45ca2d12b6a79f3358b437c84104d9
riteshk-007 <codeshorts007@gmail.com>
2024-01-19 14:31:11 +0530

    Add bcrypt and jsonwebtoken dependencies, update signup route

--- app/api/(user)/signup/route.js
+++ app/api/(user)/signup/route.js
@@ -1,1 +1,37 @@
-export  const POST = async (req)=>{}
+import prisma from '@/DB/db.config';
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server';
+export  const POST = async (req)=>{
+ try {
+       const {name, email, password} = await req.json();
+       if([name, email, password].some((filed) => filed?.trim() === "")){
+           return NextResponse.json({message : 'Please fill all the fields', status : 400})
+       }
+   
+       const hashedPassword = await bcrypt.hash(password, 10);
+       const emailCheck = await prisma.user.findUnique({
+           where:{
+               email : email,
+           }
+       })
+       if(emailCheck){
+           return NextResponse.json({message : 'Email already exists', status : 400})
+       }
+       const user = await prisma.user.create({
+           data:{
+               name,
+               email,
+               password : hashedPassword
+           }
+       })
+       return NextResponse.json(
+           {
+               status : 201,
+               message : 'User created successfully',
+               data : user
+           }
+       )
+ } catch (error) {
+     return NextResponse.json({message : error.message, status : 400})
+ }
+}
