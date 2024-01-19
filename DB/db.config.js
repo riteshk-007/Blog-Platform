14ff118120a5cec839a151a95b7fcfed129e2e42
riteshk-007 <codeshorts007@gmail.com>
2024-01-19 14:05:33 +0530

    Remove multer package and update Prisma configuration

--- DB/db.config.js
+++ DB/db.config.js
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global;
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
