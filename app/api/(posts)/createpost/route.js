32b55fd5a633aab6077a21b7154192b37149f34d
riteshk-007 <codeshorts007@gmail.com>
2024-01-20 17:22:36 +0530

    Add image field to Post model

---
+++ app/api/(posts)/createpost/route.js
@@ -0,0 +1,32 @@
+import prisma from "@/DB/db.config";
+import { NextResponse } from "next/server";
+
+export const POST = async (req) => {
+  try {
+    const { title, content, userId, image } = await req.json();
+    if ((!title || !content || !userId, !image)) {
+      return NextResponse.json({
+        message: "Please fill all the fields",
+        status: 400,
+      });
+    }
+    const newpost = await prisma.post.create({
+      data: {
+        title,
+        content,
+        userId,
+      },
+    });
+
+    return NextResponse.json({
+      status: 200,
+      message: "Post created successfully",
+      data: newpost,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      message: error.message,
+    });
+  }
+};
