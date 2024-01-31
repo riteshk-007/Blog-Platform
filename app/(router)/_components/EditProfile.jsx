2cd751f1edfd11a49242551538be2995944a6d2a
riteshk-007 <codeshorts007@gmail.com>
2024-01-31 13:15:03 +0530

    Add textarea component and update profile saving functionality

--- app/(router)/_components/EditProfile.jsx
+++ app/(router)/_components/EditProfile.jsx
@@ -10,9 +10,10 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { SquarePen } from "lucide-react";
 
-const EditProfile = ({ name, value }) => {
+const EditProfile = ({ name, value, updateInfo }) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -34,11 +35,27 @@ const EditProfile = ({ name, value }) => {
             <Label htmlFor="name" className="text-right capitalize">
               {name}
             </Label>
-            <Input id="name" defaultValue={value} className="col-span-3" />
+            {value?.length < 30 ? (
+              <Input id="name" defaultValue={value} className="col-span-3" />
+            ) : (
+              <Textarea
+                id="name"
+                defaultValue={value}
+                className="col-span-3 hide-scrollbar"
+                rows={5}
+                style={{
+                  overflow: "auto",
+                  scrollbarWidth: "none",
+                  "-ms-overflow-style": "none",
+                }}
+              />
+            )}
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save</Button>
+          <Button type="submit" onClick={updateInfo}>
+            Save
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
